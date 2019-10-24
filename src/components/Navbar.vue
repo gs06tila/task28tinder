e853172446c5c99727ef933395614a811d5328d6
Maria <mariajohnsson89@gmail.com>
2019-10-24 13:24:48 +0200

    updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,12 @@
 <template>
+<div class="navbar">
     <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="#">
     <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
     Hinder
-  </a>
-</nav>
+        </a>
+    </nav>
+</div>
 </template>
 
 <script>
@@ -13,4 +15,9 @@ export default {
 </script>
 
 <style scoped>
+
+nav {
+    color:crimson;
+}
+
 </style>
